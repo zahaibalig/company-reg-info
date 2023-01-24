568ce10b49597ff2b8380add6c577d4c7b8175e4
zahaibalig <zahaibalig@gmail.com>
2023-01-24 22:00:39 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [dataTable, setDataTable] = useState([]); 
+  const getCompaniInfo = () => {
+    Axios.get('https://data.brreg.no/enhetsregisteret/api/enheter/984851006')
+    // .then((res) => res.json() )
+    .then((response) => {
+      setDataTable(response);
+      console.log(response.data.navn);
+      })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world </h1>
+      <button onClick={getCompaniInfo}>Get company info</button>
+      {/* <input type="text" >{dataTable.navn}</input> */}
+      <h3>{dataTable.navn}</h3>
     </div>
   );
 }
