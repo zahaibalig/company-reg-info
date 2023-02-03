52fa57f9124592c0741696a5c12aef878e8acce8
zahaibalig <zahaibalig@gmail.com>
2023-02-03 23:29:31 +0100

    rule component

---
+++ src/RulesComponent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RulesComponent = () => {
+  return (
+    <View>
+      <Text>rulesComponent</Text>
+    </View>
+  )
+}
+
+export default RulesComponent
+
+const styles = StyleSheet.create({})
